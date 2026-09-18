33498c5b8c3714a35b7153f68e4149c354872c2d
Karoline Oliveira Degan <karol.degan@hotmail.com>
2026-09-17 23:36:59 -0300

    Remove a pasta inteira

--- fast_api_youtube/testes.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1IiwiZXhwIjoxNzg4MTIyNjM2fQ.PC0Mez5H2wUNXH1NxSY0bADssOZJxk52TTrnAAxoZLM"
-}
-
-requisicao = requests.get("http://localhost:8000/auth/refresh", headers=headers)
-print(requisicao)
-print(requisicao.json())
